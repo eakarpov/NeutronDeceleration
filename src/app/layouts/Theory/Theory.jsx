3408c14d086e471556191de8701a031b2f1065ae
George <eakarpov@yandex.ru>
2017-11-16 00:05:40 +0300

    Theory added

--- src/app/layouts/Theory/Theory.jsx
+++ src/app/layouts/Theory/Theory.jsx
@@ -1,53 +1,9 @@
 import React from 'react';
-import Parser from 'html-react-parser';
 
 class Theory extends React.Component {
-  constructor(props) {
-    super(props);
-    state = {};
-  }
-
-  onDocumentComplete = (pages) => {
-    this.setState({ page: 1, pages });
-  };
-
-  onPageComplete = (page) => {
-    this.setState({ page });
-  };
-
-  handlePrevious = () => {
-    this.setState({ page: this.state.page - 1 });
-  };
-
-  handleNext = () => {
-    this.setState({ page: this.state.page + 1 });
-  };
-
-  renderPagination = (page, pages) => {
-    let previousButton = <li className="previous" onClick={this.handlePrevious}><a href="#"><i className="fa fa-arrow-left"></i> Previous</a></li>;
-    if (page === 1) {
-      previousButton = <li className="previous disabled"><a href="#"><i className="fa fa-arrow-left"></i> Previous</a></li>;
-    }
-    let nextButton = <li className="next" onClick={this.handleNext}><a href="#">Next <i className="fa fa-arrow-right"></i></a></li>;
-    if (page === pages) {
-      nextButton = <li className="next disabled"><a href="#">Next <i className="fa fa-arrow-right"></i></a></li>;
-    }
-    return (
-      <nav>
-        <ul className="pager">
-          {previousButton}
-          {nextButton}
-        </ul>
-      </nav>
-    );
-  };
   render() {
-      let pagination = null;
-    if (this.state.pages) {
-      pagination = this.renderPagination(this.state.page, this.state.pages);
-    }
     return (
-      <div>{Parser(require('../../../../resources/theory.html'))}</div>
+      <div dangerouslySetInnerHTML={{ __html: require('../../../../resources/theory.html') }} />
     )
   }
 }
