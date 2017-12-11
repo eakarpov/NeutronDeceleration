de22a6c0d205602f7a2f8d8fff064ee2e4269c93
IIpocTo <allxf95@gmail.com>
2017-12-11 04:04:46 +0300

    Modeling update

--- src/app/layouts/Modeling/Modeling.jsx
+++ src/app/layouts/Modeling/Modeling.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import electron from 'electron';
 import RenderGraph from "./RenderGraph";
-
-import * as styles from './Modelling.css';
+import * as styles from './Modelling.module.scss';
 
 class Modeling extends React.Component {
 
@@ -20,15 +19,18 @@ class Modeling extends React.Component {
       path: [],
       avrg: {},
       loading: false,
+      loaded: false
     };
   }
+
   model() {
     this.setState({
       loading: true,
+      loaded: false
     });
     const matter = document.getElementById('matter').value;
     const amount = document.getElementById('amount').value;
-    const { initial, terminal } = this.state;
+    const {initial, terminal} = this.state;
     if (parseFloat(initial) - parseFloat(terminal) <= 0) {
       this.setState({
         error: true,
@@ -44,28 +46,33 @@ class Modeling extends React.Component {
         path: model.trace,
         avrg: model.avrg,
         loading: false,
+        loaded: true
       });
     });
     electron.ipcRenderer.send('model', matter, initial, terminal, amount);
   }
+
   changeInitial(e) {
     this.setState({
       initial: e.target.value,
     });
   }
+
   changeTerminal(e) {
     this.setState({
       terminal: e.target.value,
     });
   }
+
   changeAmount(e) {
     this.setState({
       amount: e.target.value,
     });
   }
+
   render() {
     return (<div>
-      {this.state.error?<mark className="secondary">Неверные данные</mark>: null}<br/>
+      {this.state.error ? <mark className="secondary">Неверные данные</mark> : null}<br/>
       <select id="matter">
         <option value="0">Вода</option>
         <option value="1">Тяжёлая вода</option>
@@ -74,49 +81,62 @@ class Modeling extends React.Component {
         <option value="4">Графит</option>
       </select><br/>
       <label>Введите начальную энергию, МэВ</label>
-      <input id="initial" onChange={this.changeInitial} /><br/>
+      <input id="initial" onChange={this.changeInitial}/><br/>
       <label>Введите конечную энергию, эВ</label>
       <input id="terminal" onChange={this.changeTerminal}/><br/>
       <label>Введите количество моделируемых нейтронов</label>
-      <input id="amount" onChange={this.changeAmount} /><br/>
-      <button onClick={this.model}>Смоделировать</button><br/>
+      <input id="amount" onChange={this.changeAmount}/><br/>
+      <button onClick={this.model}>Смоделировать</button>
+      <br/>
       {this.state.loading
         ?
-        <div>
-          <p>Моделирование...</p>
-        <div className={styles.spinner}>
-          <div className={styles.bounce1} />
-          <div className={styles.bounce2} />
-          <div className={styles.bounce3} />
-        </div>
-        </div>
-        :
-        <div>
-          <table>
-            <thead>
-            <tr>
-              <th>Параметры</th>
-              <th>E dec</th>
-              <th>log E dec</th>
-              <th>avrg length</th>
-              <th>avrg time</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-              <td>Средние значения</td>
-              <td>{this.state.avrg.eDec}</td>
-              <td>{this.state.avrg.logEDec}</td>
-              <td>{this.state.avrg.path}</td>
-              <td>{this.state.avrg.neutron_age}</td>
-            </tr>
-            </tbody>
-          </table>
-          <div>
-            {this.state.path.length}
-            <RenderGraph data={this.state.path} />
+        <div className="container">
+          <div className="col-sm-6 col-sm-offset-3">
+            <p style={{textAlign: 'center'}}><b>Пожалуйста подождите, идёт моделирование...</b></p>
           </div>
-        </div>}
+          <div className={styles.skcircle}>
+            <div className={`${styles.skcircle1} ${styles.skchild}`}/>
+            <div className={`${styles.skcircle2} ${styles.skchild}`}/>
+            <div className={`${styles.skcircle3} ${styles.skchild}`}/>
+            <div className={`${styles.skcircle4} ${styles.skchild}`}/>
+            <div className={`${styles.skcircle5} ${styles.skchild}`}/>
+            <div className={`${styles.skcircle6} ${styles.skchild}`}/>
+            <div className={`${styles.skcircle7} ${styles.skchild}`}/>
+            <div className={`${styles.skcircle8} ${styles.skchild}`}/>
+            <div className={`${styles.skcircle9} ${styles.skchild}`}/>
+            <div className={`${styles.skcircle10} ${styles.skchild}`}/>
+            <div className={`${styles.skcircle11} ${styles.skchild}`}/>
+            <div className={`${styles.skcircle12} ${styles.skchild}`}/>
+          </div>
+        </div>
+        : this.state.loaded
+          ?
+          <div>
+            <table>
+              <thead>
+              <tr>
+                <th>Параметры</th>
+                <th>Длина пробега</th>
+                <th>Возраст нейтрона</th>
+                <th>Декремент энергии</th>
+                <th>Логарифмический декремент энергии</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                <td>Средние значения</td>
+                <td>{this.state.avrg.path}</td>
+                <td>{this.state.avrg.neutron_age}</td>
+                <td>{this.state.avrg.eDec}</td>
+                <td>{this.state.avrg.logEDec}</td>
+              </tr>
+              </tbody>
+            </table>
+            <RenderGraph data={this.state.path}/>
+          </div>
+          :
+          <div/>
+      }
     </div>);
   }
 }
