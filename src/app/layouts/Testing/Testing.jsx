deee800f1ce213b6f60e3ce4b597846536a42170
IIpocTo <allxf95@gmail.com>
2017-12-14 10:24:04 +0300

    Update testing

--- src/app/layouts/Testing/Testing.jsx
+++ src/app/layouts/Testing/Testing.jsx
@@ -2,23 +2,16 @@ import React from 'react';
 import * as styles from '../Modeling/Modelling.module.scss';
 import {getTestSuite} from "../../../redux/modules/actions/test";
 import {connect} from "react-redux";
-import Checkbox from "../../components/Checkbox/Checkbox";
 
 class Testing extends React.Component {
 
   constructor(props) {
     super(props);
     this.startTest = this.startTest.bind(this);
-    this.checkAnswers = this.checkAnswers.bind(this);
     this.state = {
       initial: true,
       loading: false,
     };
-  }
-
-  checkAnswers() {
-    const checkboxes = document.getElementsByTagName('input');
-    console.log(checkboxes);
   }
 
   startTest() {
@@ -38,8 +31,8 @@ class Testing extends React.Component {
   render() {
     return (
       <div>
-        <div className="col-md-2 col-md-offset-5">
-          { this.state.initial ? <button onClick={this.startTest}>Начать тестирование</button>
+        <div>
+          {this.state.initial ? <button onClick={this.startTest}>Начать тестирование</button>
             :
             this.state.loading
               ? <div className="container">
@@ -61,15 +54,25 @@ class Testing extends React.Component {
                   <div className={`${styles.skcircle12} ${styles.skchild}`}/>
                 </div>
               </div>
-            : <div>{this.props.tests.map((el, i) =>
+              :
+              <div>{this.props.tests.map((el, i) =>
                 <div key={i}>
-                  <b>Вопрос {i+1}: </b><div dangerouslySetInnerHTML={{__html: el.question}}/>
-                  {el.answers.map((ans, j) =><div key={`outer-${j}`}>
-                      <Checkbox id={`${i}-${j}`} name={`group-${j}`} label={ans} />
-                      <br/></div>)}
+                  <b>Вопрос {i + 1}: </b>
+                  <div dangerouslySetInnerHTML={{__html: el.question}}/>
+                  <div className="input-group">
+                    {el.answers.map((ans, j) =>
+                      <div key={`outer-${j}`}>
+                        <input type="checkbox" id={`${i}-${j}`} name={`group-${j}`} tabIndex="0"/>
+                        <label htmlFor={`${i}-${j}`}>
+                          <div dangerouslySetInnerHTML={{__html: ans}}/>
+                        </label>
+                        <br/>
+                      </div>)}
+                  </div>
+                  <button>Сдать тест</button>
                 </div>)}
-                <button onClick={this.checkAnswers}>Сдать тест</button>
-              </div>}
+              </div>
+          }
         </div>
       </div>
     );
