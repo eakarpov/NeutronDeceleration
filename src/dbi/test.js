c3d9ecade8bf7079c2c46081bd9e69b633ac041a
IIpocTo <allxf95@gmail.com>
2017-12-17 21:19:15 +0300

    Fix shuffle

--- src/dbi/test.js
+++ src/dbi/test.js
@@ -1,6 +1,6 @@
 import dbi from './index';
 import async from '../helpers/asyncWrapper';
-import { encrypt, descrypt } from '../crypter/textCrypter';
+import { descrypt, encrypt } from '../crypter/textCrypter';
 
 export async function getTestSuites() {
   const db = dbi.getDb();
@@ -9,7 +9,7 @@ export async function getTestSuites() {
 
 export async function getTestSuiteQuestions(id) {
   const db = dbi.getDb();
-  const testSuite = await async(db.find, { _id: id });
+  const testSuite = await async(db.find, {_id: id});
   if (!!testSuite[0]) return false;
   testSuite.tests.map(el => el.question);
   return {
@@ -24,33 +24,57 @@ export async function getAllTests() {
   return tests.filter(el => el.question !== void 0);
 }
 
-function shuffle(arr) {
-  for (let i = 0; i < arr.length; i += 1) {
-    const index = Math.floor(Math.random() * i);
-    const swapper = arr[i];
-    arr[i] = arr[index];
-    arr[index] = swapper;
+function swap(arr, a, b) {
+  const temp = arr[b];
+  arr[b] = arr[a];
+  arr[a] = temp;
+  return arr;
+}
+
+function shuffleQuestions(questions) {
+  let shuffled = [];
+  for (let i = questions.length - 1; i > 0; i--) {
+    const index = Math.floor(Math.random() * (i + 1));
+    shuffled = swap(questions, index, i)
   }
+  return shuffled;
+}
+
+function shuffleAnswersAndIds(answers, ids) {
+  let shuffledAnswers = [];
+  let shuffledIds = [];
+  for (let i = answers.length - 1; i > 0; i--) {
+    const index = Math.floor(Math.random() * (i + 1));
+    shuffledAnswers = swap(answers, index, i);
+    shuffledIds = swap(ids, index, i);
+  }
+  return {shuffledAnswers, shuffledIds};
 }
 
 export async function generateTestSuite(...args) {
   const db = dbi.getDb();
   const testSuite = await async(db.find, {});
   const questions = testSuite.filter(el => el.question !== void 0);
-  questions.forEach(el => shuffle(el.answers));
-  shuffle(questions);
-  return questions.length > 10 ? [...questions.slice(0,10)] : questions;
+  console.log(questions);
+  for (let i = 0; i < questions.length; i++) {
+    const {shuffledAnswers, shuffledIds} = shuffleAnswersAndIds(questions[i].answers, questions[i].correctAnswersId);
+    questions[i].answers = shuffledAnswers;
+    questions[i].correctAnswersId = shuffledIds;
+  }
+  console.log(questions);
+  const shuffledQuestions = shuffleQuestions(questions);
+  return shuffledQuestions.length > 10 ? [...shuffledQuestions.slice(0, 10)] : shuffledQuestions;
 }
 
 export async function addTest(question, answers, correctAnswersId, mark) {
   const db = dbi.getDb();
-  await async(db.insert, { question, answers, correctAnswersId, mark });
+  await async(db.insert, {question, answers, correctAnswersId, mark});
   return true;
 }
 
 export async function removeTest(testSuiteId, questionId) {
   const db = dbi.getDb();
-  const testSuite = await async(db.find, { _id: testSuiteId });
+  const testSuite = await async(db.find, {_id: testSuiteId});
   if (!!testSuite[0]) return false;
   testSuite.tests.filter(el => el._id !== questionId);
   const newTestSuite = {
